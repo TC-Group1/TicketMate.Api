6ce1b4fb8f15375b205e4d2d8231fc27bbe1c2df
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-01-15 13:08:39 -0500

    Created the crud for the Insert and Delete for the UserRoles table, and updated the base request class IdDataRequest

---
+++ TicketMate.Persistence/DataRequestObjects/UserRolesRequests/UpdateRoleIdByUserId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMate.Persistence.DataRequestObjects.UserRolesRequests
+{
+    internal class UpdateRoleIdByUserId
+    {
+    }
+}
