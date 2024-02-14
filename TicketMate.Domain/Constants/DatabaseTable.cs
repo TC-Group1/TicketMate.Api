bd2b7ad7bade73645ddd8f1569f40fbe6675f05b
Daniel Aguirre <uhyeay24@gmail.com>
2024-02-14 17:14:53 -0600

    add spacing between constants

--- TicketMate.Domain/Constants/DatabaseTable.cs
+++ TicketMate.Domain/Constants/DatabaseTable.cs
@@ -3,7 +3,9 @@
     public static class DatabaseTable
     {
         public const string Users = "Users";
+
         public const string Roles = "Roles";
+        
         public const string UserRoles = "UserRoles";
     }
 }
