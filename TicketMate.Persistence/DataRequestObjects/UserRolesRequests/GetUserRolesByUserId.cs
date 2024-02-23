d23fa11dc72d7876e99bb742cceb034663f3f8ba
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-02-22 22:43:00 -0500

    Just finished making the SQL statement to view the Name of the Role, and the Full name of the User where the UserId = @userId

--- TicketMate.Persistence/DataRequestObjects/UserRolesRequests/GetUserRolesByUserId.cs
+++ TicketMate.Persistence/DataRequestObjects/UserRolesRequests/GetUserRolesByUserId.cs
@@ -8,6 +8,15 @@
 
         public object? GetParameters() => this;
 
-        public string GetSql() => $"SELECT * FROM {DatabaseTable.UserRoles} JOIN Roles ON UserRoles WHERE UserId = @userId;";
+        public string GetSql() => $"SELECT UserRoles.Id AS \"UserRoles Table ID\", " +
+                                  $"UserId, " +
+                                  $"RoleId, " +
+                                  $"Roles.Id AS \"Roles Table ID\", " +
+                                  $"Roles.Name AS \"Name of Role\", " +
+                                  $"Concat(Users.FirstName, \" \", Users.LastName) AS \"User Full Name\"\r\n" +
+                                  $"FROM {DatabaseTable.UserRoles} \r\n" +
+                                  $"JOIN Roles ON UserRoles.RoleId = Roles.Id\r\n" +
+                                  $"JOIN Users On UserRoles.UserId = Users.Id\r\n" +
+                                  $"WHERE UserId = @userId;";
     }
 }
