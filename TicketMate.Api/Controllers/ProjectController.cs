67eab2810b99acd6573827caf4f6c46bd40b7f4c
Mack McCall <mackmc9924@gmail.com>
2024-02-24 15:33:35 -0700

    added ProjectController and HttpPost InsertProject, removed using statements in insertprojecthandler

---
+++ TicketMate.Api/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketMate.Application.Abstraction;
+using TicketMate.Application.Requests.ProjectRequests.Insert;
+
+namespace TicketMate.Api.Controllers
+{
+    public class ProjectController : BaseController
+    {
+        public ProjectController(IOrchestrator orchestrator) : base(orchestrator)
+        {
+        }
+
+        [HttpPost("Project/InsertProject")]
+        public async Task InsertProject(InsertProjectRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+
+        
+    }
+}
--- TicketMate.Application/Requests/ProjectRequests/Insert/InsertProjectHandler.cs
+++ TicketMate.Application/Requests/ProjectRequests/Insert/InsertProjectHandler.cs
@@ -1,9 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
 
 namespace TicketMate.Application.Requests.ProjectRequests.Insert
