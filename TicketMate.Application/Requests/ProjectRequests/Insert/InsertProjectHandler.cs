5cd7fe77cae1ab5dea58a2ac5872d41754789e9c
Mack McCall <mackmc9924@gmail.com>
2024-02-24 14:48:43 -0700

    created InsertProjectHandler and added Project Domain Model

---
+++ TicketMate.Application/Requests/ProjectRequests/Insert/InsertProjectHandler.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
+
+namespace TicketMate.Application.Requests.ProjectRequests.Insert
+{
+    internal class InsertProjectHandler : DataRequestHandler<InsertProjectRequest>
+    {
+        public InsertProjectHandler(IDataAccess dataAccess) : base(dataAccess)
+        {
+        }
+
+        public async override Task ExecuteRequestAsync(InsertProjectRequest request)
+        {
+            try
+            {
+                var rowsAffected = await _dataAccess.ExecuteAsync(new InsertProject(request.Guid, request.Name));
+
+                if (rowsAffected <= 0)
+                {
+                    throw new OperationFailedException();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                if (ex.Message.StartsWith("Duplicate entry"))
+                {
+                    if (ex.Message.EndsWith("'Projects.Guid_UNIQUE'"))
+                    {
+                        throw new AlreadyExistsException(nameof(Project), (request.Guid, nameof(request.Guid)));
+                    }
+                }
+
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
---
+++ TicketMate.Domain/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿namespace TicketMate.Domain.Models
+{
+    public class Project
+    {
+        public Project() { }
+
+        public Project(Guid guid, string name)
+        {
+            Guid = guid;
+            Name = name;
+        }
+
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
