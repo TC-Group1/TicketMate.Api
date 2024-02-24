4190b41eb60e086c381730f999076370b6bfd593
Mack McCall <mackmc9924@gmail.com>
2024-02-24 14:25:54 -0700

    created InsertProjectRequest and added ProjectName to MaxLength Constants

---
+++ TicketMate.Application/Requests/ProjectRequests/Insert/InsertProjectRequest.cs
@@ -0,0 +1,35 @@
+﻿using TicketMate.Domain.Constants;
+using TicketMate.Domain.Validation.GuidValidation;
+using TicketMate.Domain.Validation.StringValidation;
+
+namespace TicketMate.Application.Requests.ProjectRequests.Insert
+{
+    public class InsertProjectRequest : IValidatable, IRequest
+    {
+        public InsertProjectRequest(Guid guid, string name)
+        {
+            Guid = guid;
+            Name = name;
+        }
+
+        public InsertProjectRequest()
+        {
+            
+        }
+
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new();
+
+            validator.ApplyRule(new GuidRequiredRule(Guid, nameof(Guid)));
+
+            validator.ApplyRule(new StringLengthLimitRule(Name, nameof(Name), MaxLength.ProjectName));
+
+            return validator.IsPassingAllRules;
+        }
+    }
+}
--- TicketMate.Domain/Constants/MaxLength.cs
+++ TicketMate.Domain/Constants/MaxLength.cs
@@ -5,5 +5,7 @@
         public const int Username = 50;
 
         public const int PasswordHash = 100;
+
+        public const int ProjectName = 80;
     }
 }
