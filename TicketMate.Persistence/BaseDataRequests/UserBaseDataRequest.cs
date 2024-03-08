053d37d52f3c391b5704e98d5149b672e4d25189
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-03-08 16:13:55 -0500

    Created a UserBaseDataRequest class and an UpdateUserByGuid class

---
+++ TicketMate.Persistence/BaseDataRequests/UserBaseDataRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMate.Persistence.BaseDataRequests
+{
+    public abstract class UserBaseDataRequest : IDataExecute
+    {
+        public Guid Guid { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string Email { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int IsActive { get; set; }
+
+        public UserBaseDataRequest(
+            Guid guid,
+            string passwordHash,
+            string email,
+            string avatar,
+            string firstName,
+            string lastName,
+            string phoneNumber,
+            int isActive)  
+        {
+            Guid = guid; 
+            PasswordHash = passwordHash; 
+            Email = email; 
+            Avatar = avatar;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            IsActive = isActive;
+        }
+
+        public object? GetParameters() => new {Guid, PasswordHash, Email, Avatar, FirstName, LastName, PhoneNumber, IsActive };
+
+        public abstract string GetSql();
+        
+    }
+}
---
+++ TicketMate.Persistence/DataRequestObjects/UserRequests/UpdateUserByGuid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMate.Persistence.DataRequestObjects.UserRequests
+{
+    public class UpdateUserByGuid : UserBaseDataRequest, IDataExecute
+    {
+        public UpdateUserByGuid(
+            Guid guid,
+            string passwordHash,
+            string email,
+            string avatar,
+            string firstName,
+            string lastName,
+            string phoneNumber,
+            int isActive) 
+            : base(
+                guid,
+                passwordHash,
+                email,
+                avatar,
+                firstName,
+                lastName,
+                phoneNumber,
+                isActive) { }
+        
+        public override string GetSql() => $"UPDATE {DatabaseTable.Users} " +
+                                           $"SET PASSWORDHASH = @passwordHash, " +
+                                           $"EMAIL = @email, " +
+                                           $"AVATAR = @avatar, " +
+                                           $"FIRSTNAME = @firstName, " +
+                                           $"LASTNAME = @lastName, " +
+                                           $"PHONENUMBER = @phoneNumber, " +
+                                           $"ISACTIVE = @isActive " +
+                                           $"WHERE GUID = @guid";
+        
+    }
+}
