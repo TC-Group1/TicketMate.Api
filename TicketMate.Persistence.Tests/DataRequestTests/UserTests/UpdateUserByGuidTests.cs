2b589314d1751214d8e2ee2f1eb3161afa135bb8
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-03-14 21:11:21 -0400

    Altered the second test to test if the field is exceeding the max length of the parameter

--- TicketMate.Persistence.Tests/DataRequestTests/UserTests/UpdateUserByGuidTests.cs
+++ TicketMate.Persistence.Tests/DataRequestTests/UserTests/UpdateUserByGuidTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketMate.Domain.Constants;
 using TicketMate.Persistence.DataRequestObjects.UserRequests;
 using TicketMate.Persistence.Tests.DataRequestTests.Helpers;
 
@@ -13,7 +14,7 @@ namespace TicketMate.Persistence.Tests.DataRequestTests.UserTests
     public class UpdateUserByGuidTests : BaseDataRequestTest
     {
         [Fact]
-        public async Task UpdateUserByGuid_UserExists_ShouldReturnOneRowAffected() 
+        public async Task UpdateUserByGuid_UserExists_ShouldReturnOneRowAffected()
         {
             var guid = Guid.NewGuid();
 
@@ -31,7 +32,7 @@ namespace TicketMate.Persistence.Tests.DataRequestTests.UserTests
             var newUserGuid = createTestUser.Guid;
 
             var updateRequest = new UpdateUserByGuid(
-                                        newUserGuid, 
+                                        newUserGuid,
                                         TestString.Random(),
                                         TestString.Random(),
                                         TestString.Random(15),
@@ -47,40 +48,102 @@ namespace TicketMate.Persistence.Tests.DataRequestTests.UserTests
             await _dataAccess.ExecuteAsync(new DeleteUserByGuid(newUserGuid));
         }
 
-        [Fact]
-        public async Task UpdateUserByGuid_IfWrongValuesEntered_ShouldThrow_MySqlException()
-        {
-            var guid = Guid.NewGuid();
 
+        public static IEnumerable<object[]> UpdateRequestExceedingMaxLength = new[]
+        {
+            new object[]
+            {
+                new UpdateUserByGuid(//FirstName is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName + 1),
+                    TestString.Random(MaxLength.LastName),
+                    TestString.Random(MaxLength.PhoneNumber),
+                    TestString.Random(MaxLength.Email),
+                    TestString.Random(MaxLength.Avatar),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash))
+            },
+            new object[]
+            {
+                new UpdateUserByGuid(//LastName is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName),
+                    TestString.Random(MaxLength.LastName + 1),
+                    TestString.Random(MaxLength.PhoneNumber),
+                    TestString.Random(MaxLength.Email),
+                    TestString.Random(MaxLength.Avatar),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash))
+            },
+            new object[]
+            {
+                new UpdateUserByGuid(//PhoneNumber is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName),
+                    TestString.Random(MaxLength.LastName),
+                    TestString.Random(MaxLength.PhoneNumber + 1),
+                    TestString.Random(MaxLength.Email),
+                    TestString.Random(MaxLength.Avatar),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash))
+            },
+            new object[]
+            {
+                new UpdateUserByGuid(//Email is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName),
+                    TestString.Random(MaxLength.LastName),
+                    TestString.Random(MaxLength.PhoneNumber),
+                    TestString.Random(MaxLength.Email + 1),
+                    TestString.Random(MaxLength.Avatar),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash))
+            },
+              new object[]
+            {
+                new UpdateUserByGuid(//Avatar is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName),
+                    TestString.Random(MaxLength.LastName),
+                    TestString.Random(MaxLength.PhoneNumber),
+                    TestString.Random(MaxLength.Email),
+                    TestString.Random(MaxLength.Avatar + 1),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash))
+            },
+                new object[]
+            {
+                new UpdateUserByGuid(//PasswordHash is too long
+                    Guid.NewGuid(),
+                    TestString.Random(MaxLength.FirstName),
+                    TestString.Random(MaxLength.LastName),
+                    TestString.Random(MaxLength.PhoneNumber),
+                    TestString.Random(MaxLength.Email),
+                    TestString.Random(MaxLength.Avatar),
+                    1,
+                    TestString.Random(MaxLength.PasswordHash + 1))
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(UpdateRequestExceedingMaxLength))]
+        public async Task UpdateUserByGuid_IfFieldExceedingMaxLength_ShouldThrow_MySqlException(UpdateUserByGuid invalidUpdateRequest)
+        {
             var createTestUser = new InsertUser(
-                                        guid,
-                                        TestString.Random(),
-                                        TestString.Random(),
-                                        TestString.Random(15),
-                                        TestString.Random(),
-                                        TestString.Random(),
+                                        invalidUpdateRequest.Guid,
+                                        TestString.Random(MaxLength.FirstName),
+                                        TestString.Random(MaxLength.LastName),
+                                        TestString.Random(MaxLength.PhoneNumber),
+                                        TestString.Random(MaxLength.Email),
+                                        TestString.Random(MaxLength.Avatar),
                                         1,
-                                        TestString.Random());
+                                        TestString.Random(MaxLength.PasswordHash));
 
             await _dataAccess.ExecuteAsync(createTestUser);
 
-            var newUserGuid = createTestUser.Guid;
+            await Assert.ThrowsAsync<MySqlException>(async () => await _dataAccess.ExecuteAsync(invalidUpdateRequest));
 
-            var invalidUpdateUserRequest = new UpdateUserByGuid(
-                                        newUserGuid,
-                                        TestString.Random(),
-                                        TestString.Random(),
-                                        TestString.Random(15),
-                                        TestString.Random(),
-                                        TestString.Random(),
-                                        1,
-                                        TestString.Random());
-
-            var invalidRequest = await _dataAccess.ExecuteAsync(invalidUpdateUserRequest);
-
-            await Assert.ThrowsAsync<MySqlException>(async () => await _dataAccess.ExecuteAsync(invalidUpdateUserRequest));
-
-            await _dataAccess.ExecuteAsync(new DeleteUserByGuid(newUserGuid));
+            await _dataAccess.ExecuteAsync(new DeleteUserByGuid(invalidUpdateRequest.Guid));
         }
 
     }
