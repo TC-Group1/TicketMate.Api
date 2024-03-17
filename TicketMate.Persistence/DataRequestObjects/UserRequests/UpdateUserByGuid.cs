72774835298061074f70c8b717156d7380fdf6c3
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-03-16 22:14:50 -0400

    Made some changes to the update statement and removed the UserBaseDataRequest class

--- TicketMate.Persistence/DataRequestObjects/UserRequests/UpdateUserByGuid.cs
+++ TicketMate.Persistence/DataRequestObjects/UserRequests/UpdateUserByGuid.cs
@@ -6,38 +6,47 @@ using System.Threading.Tasks;
 
 namespace TicketMate.Persistence.DataRequestObjects.UserRequests
 {
-    public class UpdateUserByGuid : UserBaseDataRequest, IDataExecute
+    public class UpdateUserByGuid :  IDataExecute
     {
-        public UpdateUserByGuid(
-            Guid guid,
-            string firstName,
-            string lastName,
-            string phoneNumber,
-            string email,
-            string avatar,
-            int isActive,
-            string passwordHash)
-            : base(
-                guid,
-                firstName,
-                lastName,
-                phoneNumber,
-                email,
-                avatar,
-                isActive,
-                passwordHash)
-        { }
-                            
+        public UpdateUserByGuid(Guid guid, string firstName, string lastName, string phoneNumber, string email, string avatar, int isActive, string passwordHash)
+        {
+            Guid = guid;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Avatar = avatar;
+            IsActive = isActive;
+            PasswordHash = passwordHash;
+        }
 
-        public override string GetSql() => $"UPDATE {DatabaseTable.Users} " +
-                                           $"SET PASSWORDHASH = @passwordHash, " +
-                                           $"EMAIL = @email, " +
-                                           $"AVATAR = @avatar, " +
-                                           $"FIRSTNAME = @firstName, " +
-                                           $"LASTNAME = @lastName, " +
-                                           $"PHONENUMBER = @phoneNumber, " +
-                                           $"ISACTIVE = @isActive " +
-                                           $"WHERE GUID = @guid";
+        public Guid Guid { get; set; }
+
+        public string? PasswordHash { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Avatar { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public int? IsActive { get; set; }
+
+        public object? GetParameters() => this;
+
+        public string GetSql() => $@"UPDATE {DatabaseTable.Users}  
+                                           SET PASSWORDHASH = COALESCE(@passwordHash, PASSWORDHASH),  
+                                           EMAIL = COALESCE(@email, EMAIL), 
+                                           AVATAR = COALESCE(@avatar, AVATAR), 
+                                           FIRSTNAME = COALESCE(@firstName, FIRSTNAME), 
+                                           LASTNAME = COALESCE(@lastName, LASTNAME), 
+                                           PHONENUMBER = COALESCE(@phoneNumber, PHONENUMBER), 
+                                           ISACTIVE = COALESCE(@isActive, ISACTIVE) 
+                                           WHERE GUID = @guid";
 
     }
 }
