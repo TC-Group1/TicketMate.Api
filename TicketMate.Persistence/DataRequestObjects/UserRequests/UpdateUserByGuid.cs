a888d4e4edbaa49e9ff1d1467177d0a1afd21218
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-03-17 21:58:46 -0400

    Made certain properties nullable and some not in UserUpdateByGuid

--- TicketMate.Persistence/DataRequestObjects/UserRequests/UpdateUserByGuid.cs
+++ TicketMate.Persistence/DataRequestObjects/UserRequests/UpdateUserByGuid.cs
@@ -22,19 +22,19 @@ namespace TicketMate.Persistence.DataRequestObjects.UserRequests
 
         public Guid Guid { get; set; }
 
-        public string? PasswordHash { get; set; }
+        public string PasswordHash { get; set; }
 
         public string? Email { get; set; }
 
         public string? Avatar { get; set; }
 
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         public string? PhoneNumber { get; set; }
 
-        public int? IsActive { get; set; }
+        public int IsActive { get; set; }
 
         public object? GetParameters() => this;
 
