6bb160b3df519ea391a05f84fe7940e0fb68e983
BraydonTGS <braydonsutherland@gmail.com>
2024-03-21 17:48:18 -0500

    Created InsertRole_Given_RoleIsInserted_Should_ReturnOneRowAffected Success, Cleaned up Duplicated in the DB table role

--- TicketMate.Persistence.Tests/DataRequestTests/RoleTests/InsertRoleByNameTests.cs
+++ TicketMate.Persistence.Tests/DataRequestTests/RoleTests/InsertRoleByNameTests.cs
@@ -1,6 +1,23 @@
-﻿namespace TicketMate.Persistence.Tests.DataRequestTests.RoleTests
+﻿using TicketMate.Persistence.DataRequestObjects.RolesRequests;
+using TicketMate.Persistence.Tests.DataRequestTests.Helpers;
+
+namespace TicketMate.Persistence.Tests.DataRequestTests.RoleTests
 {
     public class InsertRoleByNameTests : BaseDataRequestTest
     {
+        [Fact]
+        public async Task InsertRole_Given_RoleIsInserted_Should_ReturnOneRowAffected()
+        {
+            var roleName = "yoda";
+
+            var request = new InsertRoleByName(roleName);
+
+            var rowsAffected = await _dataAccess.ExecuteAsync(request);
+
+            // Delete inserted record
+            await _dataAccess.ExecuteAsync(new DeleteRoleByName(roleName));
+
+            Assert.Equal(1, rowsAffected);
+        }
     }
 }
