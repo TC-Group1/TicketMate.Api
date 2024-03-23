accd165e9696450d5aebebdec5d5b5971cb3f162
BraydonTGS <braydonsutherland@gmail.com>
2024-03-23 08:48:32 -0500

    Added DeleteRoleByName Tests - Persistance Layer

--- TicketMate.Persistence.Tests/DataRequestTests/RoleTests/DeleteRoleByNameTests.cs
+++ TicketMate.Persistence.Tests/DataRequestTests/RoleTests/DeleteRoleByNameTests.cs
@@ -1,6 +1,35 @@
-﻿namespace TicketMate.Persistence.Tests.DataRequestTests.RoleTests
+﻿using TicketMate.Persistence.DataRequestObjects.RolesRequests;
+using TicketMate.Persistence.DataRequestObjects.UserRequests;
+using TicketMate.Persistence.Tests.DataRequestTests.Helpers;
+
+namespace TicketMate.Persistence.Tests.DataRequestTests.RoleTests
 {
     public class DeleteRoleByNameTests : BaseDataRequestTest
     {
+        [Fact]
+        public async Task DeleteRoleByName_Given_RoleIsDeleted_ShouldReturn_ZeroRowsAffected()
+        {
+            Assert.Equal(0, await _dataAccess.ExecuteAsync(new DeleteRoleByName(TestString.Random(15))));
+        }
+
+        [Fact]
+        public async Task DeleteRoleByName_Given_RoleNotDeleted_ShouldReturn_OneRowAffected()
+        {
+            var roleName = TestString.Random(15);
+
+            await _dataAccess.ExecuteAsync(new InsertRoleByName(roleName));
+
+            var roleBeforeDeleting = await _dataAccess.FetchAsync(new GetRoleByName(roleName));
+
+            var rowsAffectedWhenDeleting = await _dataAccess.ExecuteAsync(new DeleteRoleByName(roleName));
+
+            var roleAfterDeleting = await _dataAccess.FetchAsync(new GetRoleByName(roleName));
+
+            Assert.NotNull(roleBeforeDeleting);
+
+            Assert.Equal(1, rowsAffectedWhenDeleting);
+
+            Assert.Null(roleAfterDeleting);
+        }
     }
 }
