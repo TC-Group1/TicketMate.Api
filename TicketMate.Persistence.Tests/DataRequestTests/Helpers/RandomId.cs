31d883a2c4872de8275f5d35d10603d5906a6937
BraydonTGS <braydonsutherland@gmail.com>
2024-03-25 16:17:50 -0500

    Created a RandomId Class for generating a RandomId to use for Testing

---
+++ TicketMate.Persistence.Tests/DataRequestTests/Helpers/RandomId.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketMate.Persistence.Tests.DataRequestTests.Helpers
+{
+    public class RandomId
+    {
+        public static int Random(int start = 25, int end = 5000)
+        {
+            var random = new Random();
+            return random.Next(start, end);
+        }
+    }
+}
