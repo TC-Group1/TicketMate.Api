9c49fa7210d5fe5df00c757cf441e01717170994
Mack McCall <mackmc9924@gmail.com>
2024-04-02 13:44:41 -0600

    Created project request folder, simple classes, and completed GetProjectByGuidResponse

---
+++ TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
+{
+    internal class GetProjectByGuidHandler : DataRequestResponseHandler<GetProjectByGuidRequest, GetProjectByGuidResponse>
+    {
+
+    }
+}
---
+++ TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
+{
+    public class GetProjectByGuidResponse
+    {
+        public GetProjectByGuidResponse(Project project) => Project = project;
+
+        public Project Project { get; set; }
+    }
+}
