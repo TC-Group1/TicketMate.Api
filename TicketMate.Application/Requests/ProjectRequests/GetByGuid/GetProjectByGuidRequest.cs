8a289b5ee5072a6c19c153574301cc4802cc3727
Mack McCall <mackmc9924@gmail.com>
2024-04-02 13:46:02 -0600

    Removed uneccesary using statements

--- TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidRequest.cs
+++ TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
+﻿namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
 {
     public class GetProjectByGuidRequest : IRequestResponse<GetProjectByGuidResponse>
     {
