dd7e1f99a2f6a70d070afe2066baa791ecca885f
Mack McCall <mackmc9924@gmail.com>
2024-04-03 14:27:50 -0600

    Completed GetProjectByGuidRequest

--- TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidRequest.cs
+++ TicketMate.Application/Requests/ProjectRequests/GetByGuid/GetProjectByGuidRequest.cs
@@ -1,7 +1,20 @@
-﻿namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
-{
-    public class GetProjectByGuidRequest : IRequestResponse<GetProjectByGuidResponse>
-    {
+﻿using TicketMate.Domain.Validation.GuidValidation;
 
+namespace TicketMate.Application.Requests.ProjectRequests.GetByGuid
+{
+    public class GetProjectByGuidRequest : IRequestResponse<GetProjectByGuidResponse>, IValidatable
+    {
+        public GetProjectByGuidRequest() { }
+
+        public GetProjectByGuidRequest(Guid projectGuid) => ProjectGuid = projectGuid;
+
+        public Guid ProjectGuid { get; set; }
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new(new GuidRequiredRule(ProjectGuid, nameof(ProjectGuid)));
+
+            return validator.IsPassingAllRules;
+        }
     }
 }
