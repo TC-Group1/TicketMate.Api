31bf7b397b2ae00befa5b0c88989f5c906f9a260
Mack McCall <mackmc9924@gmail.com>
2024-04-27 16:43:01 -0600

    Created DeleteProjectByGuid Handler, Request, and Endpoint

--- TicketMate.Api/Controllers/ProjectController.cs
+++ TicketMate.Api/Controllers/ProjectController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TicketMate.Application.Abstraction;
 using TicketMate.Application.Requests.ProjectRequests.Insert;
+using TicketMate.Application.Requests.ProjectRequests.DeleteByGuid;
+using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
 
 namespace TicketMate.Api.Controllers
 {
@@ -13,6 +15,7 @@ namespace TicketMate.Api.Controllers
         [HttpPost("Project/InsertProject")]
         public async Task InsertProject(InsertProjectRequest request) => await _orchestrator.ExecuteRequestAsync(request);
 
-        
+        [HttpDelete("Project/DeleteProjectByGuid")]
+        public async Task DeleteProjectByGuid(DeleteProjectByGuidRequest request) => await _orchestrator.ExecuteRequestAsync(request);
     }
 }
---
+++ TicketMate.Application/Requests/ProjectRequests/DeleteByGuid/DeleteProjectByGuidHandler.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
+
+namespace TicketMate.Application.Requests.ProjectRequests.DeleteByGuid
+{
+    internal class DeleteProjectByGuidHandler : DataRequestHandler<DeleteProjectByGuidRequest>
+    {
+        public DeleteProjectByGuidHandler(IDataAccess dataAccess) : base(dataAccess) { }
+
+        public async override Task ExecuteRequestAsync(DeleteProjectByGuidRequest request)
+        {
+            var rowsAffected = await _dataAccess.ExecuteAsync(new DeleteProjectByGuid(request.Guid));
+
+            if (rowsAffected <= 0)
+            {
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
