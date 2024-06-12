9719103b82dbf377d77f112133b281da6be43752
BraydonTGS <braydonsutherland@gmail.com>
2024-06-12 09:16:07 -0500

    Added InsertTicket Request and InsertTicketHandler Logic. Created Test Classes for both InsertTicketRequest and InsertTicketHandler, Added Tests. Tests Pass

---
+++ TicketMate.Application.Tests/RequestTests/TicketRequestTests/InsertTicketTests/InsertTicketHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using TicketMate.Application.Requests.TicketRequests.Insert;
+using TicketMate.Application.Tests.Helpers;
+using TicketMate.Domain.Exceptions;
+using TicketMate.Persistence.DataRequestObjects.TicketRequests;
+
+namespace TicketMate.Application.Tests.RequestTests.TicketRequestTests.InsertTicketTests
+{
+    public class InsertTicketHandlerTests : HandlerTest
+    {
+        private readonly InsertTicketRequest _request = new();
+
+        private readonly InsertTicketHandler _handler;
+
+        public InsertTicketHandlerTests() => _handler = new(_mockDataAccess.Object);
+
+        [Fact]
+        public async Task InsertTicketHandler_Given_TicketIsInserted_ShouldReturn_TaskCompletedSuccessfully()
+        {
+            var rowsAffected = 1;
+
+            SetupMockExecuteAsync<InsertTicket>(rowsAffected);
+
+            var task = _handler.ExecuteRequestAsync(_request);
+
+            await task;
+
+            Assert.True(task.IsCompletedSuccessfully);
+        }
+
+        [Fact]
+        public async Task InsertTicketHandler_Given_InsertTicketReturnsNoRowsAffected_ShouldReturn_OperationFailedException()
+        {
+            var rowsAffected = 0;
+
+            SetupMockExecuteAsync<InsertTicket>(rowsAffected);
+
+            var exception = await Record.ExceptionAsync(async () => await _handler.ExecuteRequestAsync(_request));
+
+            Assert.IsType<OperationFailedException>(exception);
+        }
+
+        [Fact]
+        public async Task InsertTicketHandler_Given_MySqlException_DuplicateGuid_ShouldThrow_AlreadyExistsException()
+        {
+            var mySqlException = MySqlExceptionHelper.Instantiate("Duplicate entry 'fdfd5be4-ae90-49df-bdd1-37ef67ce4ec8' for key 'Tickets.Guid_UNIQUE'");
+
+            _mockDataAccess.Setup(_ => _.ExecuteAsync(It.IsAny<InsertTicket>())).Throws(mySqlException);
+
+            var exception = await Record.ExceptionAsync(async () => await _handler.ExecuteRequestAsync(_request));
+
+            Assert.IsType<AlreadyExistsException>(exception);
+        }
+
+        [Fact]
+        public async Task InsertTicketHandler_Given_MySqlException_UnableToConnect_ShouldThrow_OperationFailedException()
+        {
+            var mySqlException = MySqlExceptionHelper.Instantiate("Unable to connect to any of the specified MySQL hosts.");
+
+            _mockDataAccess.Setup(_ => _.ExecuteAsync(It.IsAny<InsertTicket>())).Throws(mySqlException);
+
+            var exception = await Record.ExceptionAsync(async () => await _handler.ExecuteRequestAsync(_request));
+
+            Assert.IsType<OperationFailedException>(exception);
+        }
+    }
+}
--- TicketMate.Application/Requests/TicketRequests/Insert/InsertTicketRequest.cs
+++ TicketMate.Application/Requests/TicketRequests/Insert/InsertTicketRequest.cs
@@ -1,10 +1,33 @@
-﻿namespace TicketMate.Application.Requests.TicketRequests.Insert
+﻿using TicketMate.Domain.Constants;
+using TicketMate.Domain.Validation.GuidValidation;
+using TicketMate.Domain.Validation.StringValidation;
+
+namespace TicketMate.Application.Requests.TicketRequests.Insert
 {
     public class InsertTicketRequest : IValidatable, IRequest
     {
+        public InsertTicketRequest() { }
+
+        public Guid Guid { get; set; }
+        public Guid ProjectGuid { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
+        public int? PriorityId { get; set; }
+        public int StatusId { get; set; } = 1;
+        public Guid CreatedByUserGuid { get; set; }
+
         public bool IsValid(out Validator validator)
         {
-           throw new NotImplementedException();
+            validator = new();
+
+            validator.ApplyRule(new GuidRequiredRule(Guid, nameof(Guid)));
+            validator.ApplyRule(new GuidRequiredRule(ProjectGuid, nameof(ProjectGuid)));
+            validator.ApplyRule(new GuidRequiredRule(CreatedByUserGuid, nameof(CreatedByUserGuid)));
+
+            validator.ApplyRule(new StringRequiredRule(Title, nameof(Title)));
+            validator.ApplyRule(new StringLengthLimitRule(Title, nameof(Title), MaxLength.TicketName));
+
+            return validator.IsPassingAllRules;
         }
     }
 }
