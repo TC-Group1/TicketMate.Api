1a1be7cdd71992888e670e4f4f6b94903def8fe5
BraydonTGS <braydonsutherland@gmail.com>
2024-06-17 15:54:18 -0500

    Added Test Classes and Tests for the GetTicketByGuid Handler and Request

---
+++ TicketMate.Application.Tests/RequestTests/TicketRequestTests/GetTicketByGuidTests/GetTicketByGuidRequestValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using TicketMate.Application.Requests.TicketRequests.GetByGuid;
+
+namespace TicketMate.Application.Tests.RequestTests.TicketRequestTests.GetTicketByGuidTests
+{
+    public class GetTicketByGuidRequestValidationTests
+    {
+        [Fact]
+        public void GetTicketByGuidRequest_Given_TicketGuidNotSet_IsValid_ShouldReturn_False()
+        {
+            var request = new GetTicketByGuidRequest();
+
+            Assert.False(request.IsValid(out _));
+        }
+
+        [Fact]
+        public void GetTicketByGuidRequest_Given_TicketGuidIsEmpty_IsValid_ShouldReturn_False()
+        {
+            var request = new GetTicketByGuidRequest(Guid.Empty);
+
+            Assert.False(request.IsValid(out _));
+        }
+
+        [Fact]
+        public void GetTicketByGuidRequest_Given_TicketGuidIsValidGuid_IsValid_ShouldReturn_True()
+        {
+            var request = new GetTicketByGuidRequest(Guid.NewGuid());
+
+            Assert.True(request.IsValid(out _));
+        }
+    }
+}
