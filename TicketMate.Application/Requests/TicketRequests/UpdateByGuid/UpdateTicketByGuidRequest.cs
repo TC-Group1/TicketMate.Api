50df6ad5c0ccd95d048b3c2cf1211f80184d0d77
BraydonTGS <braydonsutherland@gmail.com>
2024-06-17 19:26:07 -0500

    Added logic and tests for the UpdateTicketByGuid Handler and Request

--- TicketMate.Application/Requests/TicketRequests/UpdateByGuid/UpdateTicketByGuidRequest.cs
+++ TicketMate.Application/Requests/TicketRequests/UpdateByGuid/UpdateTicketByGuidRequest.cs
@@ -1,10 +1,26 @@
-﻿namespace TicketMate.Application.Requests.TicketRequests.UpdateByGuid
+﻿using TicketMate.Domain.Constants;
+using TicketMate.Domain.Validation.GuidValidation;
+using TicketMate.Domain.Validation.StringValidation;
+
+namespace TicketMate.Application.Requests.TicketRequests.UpdateByGuid
 {
     public class UpdateTicketByGuidRequest : IValidatable, IRequest
     {
+        public UpdateTicketByGuidRequest() { }
+        public Guid Guid { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
+        public int? PriorityId { get; set; }
+        public int StatusId { get; set; }
         public bool IsValid(out Validator validator)
         {
-            throw new NotImplementedException();
+            validator = new();
+
+            validator.ApplyRule(new GuidRequiredRule(Guid, nameof(Guid)));
+            validator.ApplyRule(new StringRequiredRule(Title, nameof(Title)));
+            validator.ApplyRule(new StringLengthLimitRule(Title, nameof(Title), MaxLength.TicketName));
+
+            return validator.IsPassingAllRules;
         }
     }
 }
