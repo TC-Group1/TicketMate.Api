59c072aec26cbdd012255d8532f04ef486138c4f
Mack McCall <mackmc9924@gmail.com>
2024-06-24 11:20:16 -0600

    UpdateProjectByGuid Handler and Request

---
+++ TicketMate.Application/Requests/ProjectRequests/UpdateByGuid/UpdateProjectByGuidHandler.cs
@@ -0,0 +1,33 @@
+﻿using TicketMate.Domain.Constants;
+using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
+
+namespace TicketMate.Application.Requests.ProjectRequests.UpdateByGuid
+{
+    internal class UpdateProjectByGuidHandler : DataRequestHandler<UpdateProjectByGuidRequest>
+    {
+        public UpdateProjectByGuidHandler(IDataAccess dataAccess) : base(dataAccess) { }
+
+        public override async Task ExecuteRequestAsync(UpdateProjectByGuidRequest request)
+        {
+            try
+            {
+                var rowsAffected = await _dataAccess.ExecuteAsync(
+                    new UpdateProjectByGuid(
+                        request.Guid,
+                        request.Name,
+                        request.IsActive));
+
+                if (rowsAffected <= 0)
+                    throw new OperationFailedException();
+            }
+            catch (DataAccessException ex)
+            {
+                if (ex.ExceptionNumber == (MySqlExceptionNumber.DuplicateEntry))
+                    throw new AlreadyExistsException(nameof(Project), (request.Guid, nameof(request.Guid)), 
+                                                                      (request.Name, nameof(request.Name)));
+
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
---
+++ TicketMate.Application/Requests/ProjectRequests/UpdateByGuid/UpdateProjectByGuidRequest.cs
@@ -0,0 +1,27 @@
+﻿using TicketMate.Domain.Constants;
+using TicketMate.Domain.Validation.GuidValidation;
+using TicketMate.Domain.Validation.StringValidation;
+using TicketMate.Persistence.DataRequestObjects.ProjectRequests;
+
+namespace TicketMate.Application.Requests.ProjectRequests.UpdateByGuid
+{
+    public class UpdateProjectByGuidRequest : IValidatable, IRequest
+    {
+        public UpdateProjectByGuidRequest() { }
+        public Guid Guid { get; set; }
+        public string Name { get; set; } = null!;
+        public bool IsActive { get; set; }
+
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new();
+
+            validator.ApplyRule(new GuidRequiredRule(Guid, nameof(Guid)));
+            validator.ApplyRule(new StringRequiredRule(Name, nameof(Name)));
+            validator.ApplyRule(new StringLengthLimitRule(Name, nameof(Name), MaxLength.ProjectName));
+
+            return validator.IsPassingAllRules;
+        }
+    }
+}
