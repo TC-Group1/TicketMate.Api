a153cf74259f0f0aad00aa5053c9639a449002ff
Jalen Ashanti <jalenmshaw@gmail.com>
2024-07-01 21:14:03 -0400

    Added Data Request Object DeleteTicketAssignedUser

---
+++ TicketMate.Persistence/DataRequestObjects/TicketsAssignedUsersRequest/DeleteTicketAssignedUser.cs
@@ -0,0 +1,19 @@
+﻿namespace TicketMate.Persistence.DataRequestObjects.TicketsAssignedUsersRequest
+{
+    public class DeleteTicketAssignedUser : IDataExecute
+    {
+        public DeleteTicketAssignedUser(int userId, int ticketId)
+        {
+            UserId = userId;
+            TicketId = ticketId;
+        }
+
+        public int UserId { get; set; }
+        public int TicketId { get; set; }
+
+        public string GetSql() => $"DELETE FROM {DatabaseTable.TicketsAssignedUsers} WHERE UserId = @UserId AND TicketId = @TicketId";
+
+        public object? GetParameters() => this;
+
+    }
+}
