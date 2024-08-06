7f96ef60543f13eef0a1fc96994f76b12c4336dd
Jalen Ashanti <jalenmshaw@gmail.com>
2024-08-05 20:19:00 -0400

    Created new data request GetAssignedUserByTicketId, and GetAssignedUsersByTicketIdTests

---
+++ TicketMate.Persistence/DataRequestObjects/TicketsAssignedUsersRequest/GetAssignedUsersByTicketId.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TicketMate.Persistence.DataRequestObjects.TicketsAssignedUsersRequest
+{
+    public class GetAssignedUsersByTicketId : IDataFetch<Users_DTO>
+    {
+
+        public GetAssignedUsersByTicketId( int ticketId)
+        {
+            TicketId = ticketId;
+            
+        }
+
+        public int TicketId { get; set; }
+
+        public string GetSql() => $@"SELECT {DatabaseTable.Users}.* 
+                                    FROM {DatabaseTable.Users} 
+                                    LEFT JOIN {DatabaseTable.TicketsAssignedUsers} 
+                                        ON {DatabaseTable.Users}.Id = {DatabaseTable.TicketsAssignedUsers}.UserId 
+                                    WHERE {DatabaseTable.TicketsAssignedUsers}.TicketId = @TicketId;";
+
+        public object? GetParameters() => this;
+    }
+}
