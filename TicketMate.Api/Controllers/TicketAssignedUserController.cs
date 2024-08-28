7035939743e5ac22361f83548e19ed4cb8200f93
Jalen Ashanti <jalenmshaw@gmail.com>
2024-08-28 18:46:01 -0400

    API:
    Created Controller for TicketAsssignedUser
    
    Created Endpoint for InsertTicketAssignedUser and DeleteTicketAssignedUser

---
+++ TicketMate.Api/Controllers/TicketAssignedUserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketMate.Application.Abstraction;
+using TicketMate.Application.Requests.TicketAssignedUsersRequests.Delete;
+using TicketMate.Application.Requests.TicketAssignedUsersRequests.Insert;
+
+namespace TicketMate.Api.Controllers
+{
+	public class TicketAssignedUserController : BaseController
+	{
+		public TicketAssignedUserController(IOrchestrator orchestrator) : base(orchestrator) { }
+
+		[HttpPost("TicketAssignedUser/InsertTicketAssignedUser")]
+		public async Task InsertTicketAssignedUser(InsertTicketAssignedUserRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+
+		[HttpDelete("TicketAssignedUser/DeleteTicketAssignedUser")]
+		public async Task DeleteTicketAssignedUser(DeleteTicketAssignedUserRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+	}
+}
